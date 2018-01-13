2d5060fa7bdeeddf460990d1743d76705f70bd34
AllanDragoon <AllanDragoon@gmail.com>
2018-01-13 18:59:36 +0800

    Implement ApartmentContour.cs

--- Develop/RoomDetector/Source/LS.MapClean.Addin/Main/ApartmentContour.cs
+++ Develop/RoomDetector/Source/LS.MapClean.Addin/Main/ApartmentContour.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
 using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
 using LS.MapClean.Addin.Utils;
 using LS.MapClean.Addin.Algorithms;
+using NetTopologySuite.Geometries;
 
 namespace LS.MapClean.Addin.Main
 {
     internal class ApartmentContour
     {
-        public static IEnumerable<DBPoint> CalcContour(Document doc, IEnumerable<ObjectId> objectIdsList)
+        public static List<Point2d> CalcContour(Document doc, IEnumerable<ObjectId> objectIdsList)
         {
             var newCreatedIds = new List<ObjectId>();
             IEnumerable<ObjectId> polygonIds = new List<ObjectId>();
@@ -82,16 +85,122 @@ namespace LS.MapClean.Addin.Main
                         newCreatedIds.Remove(pair.Key);
                         newCreatedIds.AddRange(pair.Value);
                     }
-
-                    polygonIds = checkIds;
                 }
 
-                // 2. Make polygons.
-                doc.Editor.WriteMessage("开始造闭合多边形...");
-                var resultIds = new List<ObjectId>();
-                // 3. Union all polygons
-                // 4. Get largest are polygon which is the apartment's contour
+                polygonIds = checkIds;
             }
+
+            // 2. Make polygons.
+            doc.Editor.WriteMessage("开始造闭合多边形...");
+            var resultIds = new List<ObjectId>();
+            if (polygonIds.Any())
+            {
+                var color = Color.FromColorIndex(ColorMethod.ByAci, 3); // Green
+                ObjectId layerId = LayerUtils.AddNewLayer(doc.Database, "temp-poly", "Continuous", color);
+
+                using (var tolerance = new SafeToleranceOverride())
+                using (var waitCursor = new WaitCursorSwitcher())
+                {
+                    var polygons = MinimalLoopSearcher2.Search(polygonIds, doc);
+                    using (var transaction = doc.Database.TransactionManager.StartTransaction())
+                    {
+                        var modelspaceId = SymbolUtilityServices.GetBlockModelSpaceId(doc.Database);
+                        var modelspace = (BlockTableRecord)transaction.GetObject(modelspaceId, OpenMode.ForWrite);
+                        foreach (var polyline in polygons)
+                        {
+                            polyline.Color = color;
+                            polyline.LayerId = layerId;
+                            var id = modelspace.AppendEntity(polyline);
+                            resultIds.Add(id);
+                            transaction.AddNewlyCreatedDBObject(polyline, true);
+                        }
+                        transaction.Commit();
+                    }
+                }
+            }
+
+            using (var tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                foreach (var newCreatedId in newCreatedIds)
+                {
+                    if (newCreatedId.IsErased)
+                        continue;
+                    var dbObj = tr.GetObject(newCreatedId, OpenMode.ForWrite);
+                    dbObj.Erase();
+                }
+
+                ////////////////////////////////////////////////////////////////////////////////////////
+                tr.Commit();
+            }
+            doc.Editor.WriteMessage("造多边形成功！");
+            // 3. Union all polygons
+            var database = doc.Database;
+            var partitioner = new DrawingPartitioner(database);
+            partitioner.Check(resultIds);
+            // 4. Get largest are polygon which is the apartment's contour
+            var polylines = new List<Polyline>();
+            foreach (var region in partitioner.IsolatedRegions)
+            {
+                var polyline = new Polyline(region.Contour.Count);
+                int i = 0;
+                foreach (var point in region.Contour)
+                {
+                    polyline.AddVertexAt(i++, new Point2d(point.X, point.Y), 0, 0, 0);
+                }
+                polyline.Closed = true;
+                polylines.Add(polyline);
+            }
+            polylines.Sort((poly1, poly2) =>
+            {
+                if (poly1.Area > poly2.Area)
+                    return -1;
+                return 1;
+            });
+            Polyline largestPolyline = polylines.FirstOrDefault();
+            var resultPoints = new List<Point2d>();
+            if (largestPolyline != null)
+            {
+                resultPoints = CurveUtils.GetDistinctVertices2D(largestPolyline, null);
+                if (resultPoints[0] != resultPoints[resultPoints.Count - 1])
+                    resultPoints.Add(resultPoints[0]);
+                var clockwise = ComputerGraphics.ClockWise2(resultPoints.ToArray());
+                if (clockwise)
+                {
+                    resultPoints.Reverse();
+                }
+
+                using (var transaction = database.TransactionManager.StartTransaction())
+                {
+                    var modelspaceId = SymbolUtilityServices.GetBlockModelSpaceId(database);
+                    var modelspace = (BlockTableRecord)transaction.GetObject(modelspaceId, OpenMode.ForWrite);
+
+                    var color = Color.FromColorIndex(ColorMethod.ByAci, 3); // Green
+                    ObjectId layerId = LayerUtils.AddNewLayer(doc.Database, "temp-poly", "Continuous", color);
+                    largestPolyline.Color = color;
+                    largestPolyline.LayerId = layerId;
+                    modelspace.AppendEntity(largestPolyline);
+                    transaction.AddNewlyCreatedDBObject(largestPolyline, add: true);
+                    
+                    transaction.Commit();
+                }
+            }
+
+            // 5. Delete the polygons of resultIds
+            using (var tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                foreach (var newCreatedId in resultIds)
+                {
+                    if (newCreatedId.IsErased)
+                        continue;
+                    var dbObj = tr.GetObject(newCreatedId, OpenMode.ForWrite);
+                    dbObj.Erase();
+                }
+
+                ////////////////////////////////////////////////////////////////////////////////////////
+                tr.Commit();
+            }
+
+            return resultPoints;
         }
     }
 }
